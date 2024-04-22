69cc83c76429d886628d124ab07c352c40abadfe
ZhuangZhou939 <158232100+Ranceeeeee@users.noreply.github.com>
2024-04-23 00:24:51 +0800

    Main.cpp
    change some global variables and add the headfiles

--- Main.cpp
+++ Main.cpp
@@ -6,23 +6,23 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include "attack.h"
 using namespace std;
 
+int map_size;
 int elixir=0;  //cost
 int elixir_max=6;
-int turn=1;  //number of turns
+int turn=0;  //number of turns
 struct Ship {
-	int x;
-	int y;
-	bool r;  //direction of the ship, 0 for being horizontal and 1 for vertical
+	int x1, y1, x2, y2;
 	int hp;
 };
-map<string, Ship> playerships;  //to store player's ships
+map<string,  Ship> playerships;  //to store player's ships
 map<string, Ship> enemyships;  //opponent's ships
 
 int main() {
 	//输入转换函数，获取用户的第一次输入
-
+	turn++;
 	//Big turn for player and enemy
 	while (true) {
 		//player turn
