678c6fc4a3154fddc6ce2fb4c4c682c6a4cc608a
ZhuangZhou939 <158232100+Ranceeeeee@users.noreply.github.com>
2024-04-23 19:20:12 +0800

    attack.h
    change the format

--- attack.h
+++ attack.h
@@ -1,10 +1,15 @@
 // attack_detection.h 
-#ifndef ATTACK_H 
-#define ATTACK_H
+#include <string>
+using namespace std;
+
+//输入打击对象，打击对象的地图，点的位置
+void attack_detect(string target, string** ptr, string** ptr_seen, int x, int y, int damage);
 //把报废船或者建筑更新到地图上
 void broken_update(string target, string item, string** ptr);
 //检测目标玩家的船或者建筑是否报废
 void broken_detect(string target, string item, string** ptr);
-//输入打击对象，打击对象的地图，点的位置
-void attack_detect(string target, string** ptr, int x, int y);
-#endif 
+//使用攻击类型
+void attack_input_conversion(string target, string** ptr, string** ptr_seen, string command, int x, int y, int damage);
+//特殊技能
+void skills(string target, string** ptr, string** ptr_seen, string command, int x1, int y1, int x2, int y2);
+
