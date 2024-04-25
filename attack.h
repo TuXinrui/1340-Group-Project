3dc77368d2a5112f64f09989891490c70263df08
ZhuangZhou939 <158232100+Ranceeeeee@users.noreply.github.com>
2024-04-25 15:23:35 +0800

    attack.h
    the parameters changed becuase there are now 4 pointers for 4 maps

--- attack.h
+++ attack.h
@@ -3,13 +3,13 @@
 using namespace std;
 
 //输入打击对象，打击对象的地图，点的位置
-void attack_detect(string target, string** ptr, string** ptr_seen, int x, int y, int damage);
+void attack_detect(string target, string** ptr_real, string** ptr_seen, int x, int y, int damage);
 //把报废船或者建筑更新到地图上
-void broken_update(string target, string item, string** ptr);
+void broken_update(string target, string item, string** ptr_seen);
 //检测目标玩家的船或者建筑是否报废
-void broken_detect(string target, string item, string** ptr);
+void broken_detect(string target, string item, string** ptr_seen);
 //使用攻击类型
-void attack_input_conversion(string target, string** ptr, string** ptr_seen, string command, int x, int y, int damage);
+void attack_input_conversion(string target, string** ptr_real, string** ptr_seen, string command, int x, int y, int damage);
 //特殊技能
-void skills(string target, string** ptr, string** ptr_seen, string command, int x1, int y1, int x2, int y2);
+void skills(string target, string** ptr_real, string** ptr_seen, string command, int x1, int y1, int x2, int y2);
 
