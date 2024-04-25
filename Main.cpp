f95a524cd84fc24d8fa4529e705446b2b8a36956
ZhuangZhou939 <158232100+Ranceeeeee@users.noreply.github.com>
2024-04-25 15:20:17 +0800

    Main.cpp
    Change the while structure

--- Main.cpp
+++ Main.cpp
@@ -13,20 +13,27 @@ using namespace std;
 
 
 int main() {
-	//输入转换函数，获取用户的第一次输入
-	turn++;
-	
-	//Big turn for player and enemy
-	while (true) {
-		//player turn
-		while (true) {
-			//输入转换函数xxxxxxxxxx
-		}
-		//enemy turn
-		while (true) {
-			//输入怪物函数
-		}
+	input_conversion(0);
+	//it is a new game
+	if (turn == 0) {
+		//defines 4 pointers towards 4 maps
+		string** player_real = new string * [map_size];
+		string** player_seen = new string * [map_size];
+		string** enemy_real = new string * [map_size];
+		string** enemy_seen = new string * [map_size];
 		turn++;
 	}
-	
+	else {
+		//读档的情况
+	}
+	//Big turn for player and enemy
+	while (true) {
+		input_conversion(player_real, player_seen, enemy_real, enemy_seen);
+		//enemy turn
+		turn++;
+		if (turn == -1) {
+			cout << "game ends" << endl;
+			break;
+		}
+	}
 }
