daf857e37c23750896c06aca10da1b7c808de482
ZhuangZhou939 <158232100+Ranceeeeee@users.noreply.github.com>
2024-04-27 23:39:20 +0800

    support.h

--- support.h
+++ support.h
@@ -5,22 +5,24 @@ using namespace std;
 
 extern int map_size;
 extern int attack_buff[2];
-extern int elixir[2];  //cost
+extern int elixir[2];  //cost  elixir[0]Íæ¼Ò·ÑÓÃ
 extern int elixir_max[2];
-extern int elixir_increament[2];
 extern int turn;  //number of turns
 extern int torpedo_max[2];
 extern int torpedo_remain;
-
-extern struct Ship {
+extern int elixir_increament[2];
+struct Ship {
 	int x1, y1, x2, y2;
 	int hp;
+	int hp_max;
 	bool status;
 };
-extern struct Cor {
+struct Cor {
 	int x, y;
 };
-
-extern std::map<std::string, Ship> playerships;
-extern std::map<std::string, Ship> enemyships;
+extern vector <Cor> damaged_grids;
+extern vector <Cor> empty_grids;  //"-"
+extern vector<Cor> hit_grids;  //"X"
+extern map<std::string, Ship> playerships;
+extern map<std::string, Ship> enemyships;
 
