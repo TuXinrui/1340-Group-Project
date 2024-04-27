7946f9c92f0cc7543fb5101b34101fb4c61204de
TuXinrui <161405151+TuXinrui@users.noreply.github.com>
2024-04-27 15:01:11 +0800

    inputtransform.h

---
+++ inputtransform.h
@@ -0,0 +1,6 @@
+// inputtransform.h 
+#include <string>
+using namespace std;
+
+//初步处理用户信息
+void inputtransform(string** player_map_seen, string** player_map_real, string** enemy_map_seen,string** enemy_map_real);
