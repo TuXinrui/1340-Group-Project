c7508af9ec7016e74a2b4c6e0988caa7c7695bbc
ZhuangZhou939 <158232100+Ranceeeeee@users.noreply.github.com>
2024-04-28 13:56:29 +0800

    enemy_ai.cpp

--- enemy_ai.cpp
+++ enemy_ai.cpp
@@ -31,30 +31,28 @@ void enemy_container(string** ptr_seen) {
 }
 
 void enemy_execute(string** player_real, string** player_seen, string** enemy_real,string** enemy_seen) {
-	//进行普通打击
-	for (torpedo_remain; torpedo_remain>0; torpedo_remain--) {
-		srand((unsigned)time(NULL));
-		int chosen_grid = rand() % empty_grids.size() + 1;
-		int x = empty_grids[chosen_grid].x;
-		int y = empty_grids[chosen_grid].y;
-		attack_detect("player", player_real, player_seen, x, y, 1);
-	}
-	//紧急
+	//紧急移动
 	if ((enemyships["H"].hp <= 5) and (enemyships["H"].status == 1) and (elixir[1] >= 5)) {
-		while (true) {
+		int count = 0;
+		while (count < 300) {
 			srand((unsigned)time(NULL));
 			int x1 = rand() % map_size + 1;
 			int y1 = rand() % map_size + 1;
-			for (int i = 0; i < 7; i++) {
-				for (int j = 0; j < 3; j++) {
-					if (enemy_seen[i][j] != "-")  continue;
+			for (int i = x1; i < (enemyships["H"].x2 - enemyships["H"].x1); i++) {
+				for (int j = y1; j < (enemyships["H"].y2 - enemyships["H"].y1); j++) {
+					if (enemy_seen[i][j] != "-") {
+						count++;
+						continue;
+					}
 				}
 			}
-			move();
+			skills("enemy", enemy_real, enemy_seen, "move", enemyships["H"].x1, enemyships["H"].y1, x1, y1);
+			cout << "enemy move finished" << endl;
+			elixir[1] -= 5;
 		}
 	}
-	//费用少情况下
-	else if (elixir[1] >= 1 and elixir[1] <= 2) {
+	//费用少:治疗
+	if (elixir[1] >= 2 and elixir[1] <= 3) {
 		//如果有受伤
 		for (string i : {"H", "h", "K", "k", "Q", "q"}) {
 			if (((enemyships[i].hp_max - enemyships[i].hp) >= 3)and enemyships[i].status == 1) {
@@ -64,7 +62,150 @@ void enemy_execute(string** player_real, string** player_seen, string** enemy_re
 			}
 		}
 	}
-	else if (elixir[1] >= 3 and elixir[1] <= 5) {
-
+	//费用多:炸弹
+	if (elixir[1] >= 4) {
+		srand((unsigned)time(NULL));
+		int direction = rand() % 2;
+		//使用3x3炸弹
+		if (direction == 0) {
+			if (hit_grids.size() > 0) {
+				Cor target = hit_grids[0];
+				if (target.x + 1 > map_size) {
+					target.x--;
+				}
+				if (target.x - 1 < 1) {
+					target.x++;
+				}
+				if (target.y + 1 > map_size) {
+					target.y--;
+				}
+				if (target.y - 1 < 1) {
+					target.y++;
+				}
+				for (int i = target.x - 1; i <= target.x + 1; i++) {
+					for (int j = target.y - 1; j <= target.y + 1; j++) {
+						attack_detect("player", player_real, player_seen, i, j, 1);
+						forbid_grids.push_back({ i,j });
+					}
+				}
+				elixir[1] -= 3;
+			}
+			
+		}
+	}
+	//进行普通打击
+	for (torpedo_remain; torpedo_remain > 0; torpedo_remain--) {
+		//有"X"并且forbid是0
+		if ((hit_grids.size() > 0) and (forbid_grids.size() == 0)) {
+			srand((unsigned)time(NULL));
+			int chosen_grid = rand() % hit_grids.size() + 1;
+			int x = empty_grids[chosen_grid].x;
+			int y = empty_grids[chosen_grid].y;
+			attack_detect("player", player_real, player_seen, x, y, 1);
+			forbid_grids.push_back({ x,y });
+			continue;
+		}
+		//有"X"并且forbid也有
+		else if ((hit_grids.size() > 0)) {
+			vector<Cor>::iterator itr1;
+			vector<Cor>::iterator itr2;
+			bool hit = 0;
+			//遍历hit容器，找出不在一回合打击过的点
+			for (itr1 = hit_grids.begin(); itr1 != hit_grids.end(); itr1++) {
+				bool valid = 1;
+				for (itr2 = forbid_grids.begin(); itr2 != forbid_grids.end(); itr2++) {
+					if (((*itr1).x == (*itr2).x) or ((*itr1).y = (*itr2).y)) {
+						valid = 0;
+						break;
+					}
+				}
+				if (valid) {
+					attack_detect("player", player_real, player_seen, (*itr1).x, (*itr1).y, 1);
+					hit = 1;
+					break;
+				}
+			}
+			//全部X都打过了
+			if (hit == 0) {
+				//打hit的右边
+				for (itr1 = hit_grids.begin(); itr1 != hit_grids.end(); itr1++) {
+					bool valid = 1;
+					if ((*itr1).x + 1 > map_size)  continue;
+					for (itr2 = forbid_grids.begin(); (itr2 != forbid_grids.end()); itr2++) {
+						if (((*itr1).x + 1 == (*itr2).x) or ((*itr1).y == (*itr2).y)) {
+							valid = 0;
+							break;
+						}
+					}
+					if (valid) {
+						attack_detect("player", player_real, player_seen, (*itr1).x + 1, (*itr1).y, 1);
+						hit = 1;
+						break;
+					}
+				}
+				//打hit的左边
+				if (hit == 0) {
+					for (itr1 = hit_grids.begin(); itr1 != hit_grids.end(); itr1++) {
+						bool valid = 1;
+						if ((*itr1).x - 1 < 1)  continue;
+						for (itr2 = forbid_grids.begin(); (itr2 != forbid_grids.end()); itr2++) {
+							if (((*itr1).x - 1 == (*itr2).x) or ((*itr1).y == (*itr2).y)) {
+								valid = 0;
+								break;
+							}
+						}
+						if (valid) {
+							attack_detect("player", player_real, player_seen, (*itr1).x - 1, (*itr1).y, 1);
+							hit = 1;
+							break;
+						}
+					}
+				}
+				//打hit的上边
+				if (hit == 0) {
+					for (itr1 = hit_grids.begin(); itr1 != hit_grids.end(); itr1++) {
+						bool valid = 1;
+						if ((*itr1).y + 1 > map_size)  continue;
+						for (itr2 = forbid_grids.begin(); (itr2 != forbid_grids.end()); itr2++) {
+							if (((*itr1).x == (*itr2).x) or ((*itr1).y + 1 == (*itr2).y)) {
+								valid = 0;
+								break;
+							}
+						}
+						if (valid) {
+							attack_detect("player", player_real, player_seen, (*itr1).x, (*itr1).y + 1, 1);
+							hit = 1;
+							break;
+						}
+					}
+				}
+				//打hit的下边
+				if (hit == 0) {
+					for (itr1 = hit_grids.begin(); itr1 != hit_grids.end(); itr1++) {
+						bool valid = 1;
+						if ((*itr1).y - 1 < 1)  continue;
+						for (itr2 = forbid_grids.begin(); (itr2 != forbid_grids.end()); itr2++) {
+							if (((*itr1).x == (*itr2).x) or ((*itr1).y - 1 == (*itr2).y)) {
+								valid = 0;
+								break;
+							}
+						}
+						if (valid) {
+							attack_detect("player", player_real, player_seen, (*itr1).x, (*itr1).y - 1, 1);
+							hit = 1;
+							break;
+						}
+					}
+				}
+			}
+		}
+		//随机目标
+		else {
+			srand((unsigned)time(NULL));
+			int chosen_grid = rand() % empty_grids.size() + 1;
+			int x = empty_grids[chosen_grid].x;
+			int y = empty_grids[chosen_grid].y;
+			attack_detect("player", player_real, player_seen, x, y, 1);
+		}
 	}
 }
