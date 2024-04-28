f92ec0d37210f046054bb4fa2beef3079e4a77c1
ZhuangZhou939 <158232100+Ranceeeeee@users.noreply.github.com>
2024-04-28 13:56:49 +0800

    support.h

--- support.h
+++ support.h
@@ -1,32 +1,29 @@
-#ifndef SUPPORT_H
-#define SUPPORT_H
-
-#include <vector>
+// support.h 
 #include <map>
 #include <string>
-
-struct Ship {
-    int x1, y1, x2, y2;
-    int hp;
-    bool status;
-};
-
-struct Cor {
-    int x, y;
-};
+using namespace std;
 
 extern int map_size;
 extern int attack_buff[2];
-extern int elixir[2];
+extern int elixir[2];  //cost  elixir[0]玩家费用
 extern int elixir_max[2];
-extern int elixir_increament[2];
-extern int turn;
+extern int turn;  //number of turns
 extern int torpedo_max[2];
 extern int torpedo_remain;
-extern std::string dialogue;
+extern int elixir_increament[2];
+struct Ship {
+	int x1, y1, x2, y2;
+	int hp;
+	int hp_max;
+	bool status;
+};
+struct Cor {
+	int x, y;
+};
+extern vector <Cor> damaged_grids;
+extern vector <Cor> empty_grids;  //"-"
+extern vector<Cor> hit_grids;  //"X"
+extern vector<Cor> forbid_grids; //怪物一回合打到的点
+extern map<std::string, Ship> playerships;
+extern map<std::string, Ship> enemyships;
 
-extern std::map<std::string, Ship> playerships;
-extern std::map<std::string, Ship> enemyships;
-extern std::vector<Cor> damaged_grids;
-
-#endif
