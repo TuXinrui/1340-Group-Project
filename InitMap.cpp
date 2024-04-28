a20917a017f13ee6aa86f95e7f1efe3b656d9e10
AlexBao1218 <164132418+AlexBao1218@users.noreply.github.com>
2024-04-28 19:59:36 +0800

    Update InitMap.cpp

--- InitMap.cpp
+++ InitMap.cpp
@@ -15,19 +15,7 @@ std::map<std::string, Ship> playerships;
 std::map<std::string, Ship> enemyships;
 int map_size = 15;
 
-std::tuple<std::string**, std::string**, std::string**, std::string**> InitMap() {
-    // 动态分配map_size*map_size的二维字符串数组
-    std::string** playerMap = new std::string*[map_size];
-    std::string** playerSeaMap = new std::string*[map_size];
-    std::string** fogMap = new std::string*[map_size];
-    std::string** enemyMap = new std::string*[map_size];
-    for(int i = 0; i < map_size; i++) {
-        playerMap[i] = new std::string[map_size];
-        playerSeaMap[i] = new std::string[map_size];
-        fogMap[i] = new std::string[map_size];
-        enemyMap[i] = new std::string[map_size];
-    }
-
+void InitMap(std::string** playerMap, std::string** playerSeaMap, std::string** fogMap, std::string** enemyMap) {
     // 初始化地图上的水域
     for(int i = 0; i < map_size; i++) {
         for(int j = 0; j < map_size; j++) {
@@ -42,32 +30,67 @@ std::tuple<std::string**, std::string**, std::string**, std::string**> InitMap()
     std::vector<std::tuple<std::string, int, int, int>> ships = {
         {"H", 10, 3, 7}, {"h", 10, 3, 7}, {"Q", 3, 1, 5}, {"q", 3, 1, 5}, {"K", 2, 1, 4}, {"k", 2, 1, 4}
     };
-    int x = 0, y = 0;
     for(auto& ship : ships) {
         std::string shipName = std::get<0>(ship);
         int shipHP = std::get<1>(ship);
         int shipHeight = std::get<2>(ship);
         int shipWidth = std::get<3>(ship);
         
-        // 检查船只是否超出地图边界
-        if(x + shipHeight > map_size || y + shipWidth > map_size) {
-            std::cout << "船只 " << shipName << " 放置失败,超出地图边界!" << std::endl;
-            continue;
-        }
-        
-        // 放置船只
-        for(int i = x; i < x + shipHeight; i++) {
-            for(int j = y; j < y + shipWidth; j++) {
-                playerMap[i][j] = shipName;
-                playerSeaMap[i][j] = shipName;
+        while(true) {
+            int x, y;
+            std::string direction;
+            std::cout << "请输入船只 " << shipName << " 的左下角放置坐标x y (用空格分隔): ";
+            std::cin >> x >> y;
+            std::cout << "请输入船只 " << shipName << " 的放置方向 (h为横向,v为纵向): ";
+            std::cin >> direction;
+
+            // 检查船只是否超出地图边界
+            if(direction == "h" && y + shipWidth > map_size) {
+                std::cout << "船只 " << shipName << " 放置失败,超出地图边界!" << std::endl;
+                continue;
             }
-        }
-        playerships[shipName] = {x, y, x+shipHeight-1, y+shipWidth-1, shipHP, true};
-        
-        y += shipWidth + 1;
-        if(y >= map_size) {
-            x += 3;
-            y = 0;
+            if(direction == "v" && x + shipHeight > map_size) {
+                std::cout << "船只 " << shipName << " 放置失败,超出地图边界!" << std::endl;
+                continue;
+            }
+            
+            // 检查船只是否与其他船只重叠
+            bool isOverlap = false;
+            if(direction == "h") {
+                for(int j = y; j < y + shipWidth; j++) {
+                    if(playerMap[x][j] != "-") {
+                        isOverlap = true;
+                        break;
+                    }
+                }
+            } else {
+                for(int i = x; i < x + shipHeight; i++) {
+                    if(playerMap[i][y] != "-") {
+                        isOverlap = true;
+                        break;
+                    }
+                }
+            }
+            if(isOverlap) {
+                std::cout << "船只 " << shipName << " 放置失败,与其他船只重叠!" << std::endl;
+                continue;
+            }
+
+            // 放置船只
+            if(direction == "h") {
+                for(int j = y; j < y + shipWidth; j++) {
+                    playerMap[x][j] = shipName;
+                    playerSeaMap[x][j] = shipName;
+                }
+                playerships[shipName] = {x, y, x, y+shipWidth-1, shipHP, true};
+            } else {
+                for(int i = x; i < x + shipHeight; i++) {
+                    playerMap[i][y] = shipName;
+                    playerSeaMap[i][y] = shipName;
+                }
+                playerships[shipName] = {x, y, x+shipHeight-1, y, shipHP, true};
+            }
+            break;
         }
     }
 
@@ -85,32 +108,42 @@ std::tuple<std::string**, std::string**, std::string**, std::string**> InitMap()
         while(true) {
             int x = rand() % (map_size - shipHeight + 1);
             int y = rand() % (map_size - shipWidth + 1);
+            std::string direction = rand() % 2 == 0 ? "h" : "v";
 
             // 检查船只是否与其他船只重叠
             bool isOverlap = false;
-            for(int i = x; i < x + shipHeight; i++) {
+            if(direction == "h") {
                 for(int j = y; j < y + shipWidth; j++) {
-                    if(enemyMap[i][j] != "-") {
+                    if(enemyMap[x][j] != "-") {
                         isOverlap = true;
                         break;
                     }
                 }
-                if(isOverlap) break;
+            } else {
+                for(int i = x; i < x + shipHeight; i++) {
+                    if(enemyMap[i][y] != "-") {
+                        isOverlap = true;
+                        break;
+                    }
+                }
             }
             if(isOverlap) {
                 continue;
             }
 
             // 放置船只
-            for(int i = x; i < x + shipHeight; i++) {
+            if(direction == "h") {
                 for(int j = y; j < y + shipWidth; j++) {
-                    enemyMap[i][j] = shipName;
+                    enemyMap[x][j] = shipName;
                 }
+                enemyships[shipName] = {x, y, x, y+shipWidth-1, shipHP, true};
+            } else {
+                for(int i = x; i < x + shipHeight; i++) {
+                    enemyMap[i][y] = shipName;
+                }
+                enemyships[shipName] = {x, y, x+shipHeight-1, y, shipHP, true};
             }
-            enemyships[shipName] = {x, y, x+shipHeight-1, y+shipWidth-1, shipHP, true};
             break;
         }
     }
-
-     return {playerMap, playerSeaMap, fogMap, enemyMap};
 }
